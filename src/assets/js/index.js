fc235e1332acf6171f2a86ec64ea1069cbc1b37b
Meekiavelique <meekiavelique@gmail.com>
2024-10-26 18:09:11 +0200

    Pushing code

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,7 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Vous voyez ce mec ? Tu sais ce mec...", "author": "Seb la frite" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
